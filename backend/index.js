5bd716e89557d58ba7d96041e006b0014214c26b
Sammam-Sohail <sammam.sohail@analogmutations.com>
2023-05-11 00:02:00 +0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+
+const database = {
+    "101" : "Sammam Sohail",
+    "102" : "Mohib Rehman",
+    "103" : "Midhat",
+    "104" : "Mohib Tariq",
+}
+
+
+app.get("/start", async (req, res) => {
+	let { message } = req.body
+
+
+    res.send("All Connections Established!!")
+})
+
+app.post("/login/:id", async (req, res) => {
+	const id = req.params.id
+    
+    if (id in database){
+        const person = database[id]
+        res.status(200).send(`Welcome ${person}`)
+    }
+    else{
+        res.status(404).send("Invalid id")
+    } 
+
+})
