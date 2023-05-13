387a0966ec5da5829bf5c1a5ff348728c664dff3
Sammam-Sohail <sammam.sohail@analogmutations.com>
2023-05-13 15:09:30 +0500

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors());
 
 // CI pipeline 
 
